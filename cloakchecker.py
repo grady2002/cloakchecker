c734cb827749dbd11c9c68bf1786b19382630b1d
Aatif Mastan <aatifmastan644@gmail.com>
2023-11-02 15:55:50 +0530

    now using ip-api for location

--- cloakchecker.py
+++ cloakchecker.py
@@ -1,18 +1,34 @@
 from os import system
+from json import loads
 
 INTERFACE = "eth0"
-LAN_IP_COMMAND = f"echo $(ifconfig {INTERFACE} | grep inet) > tmp"
-WAN_IP_COMMAND = "echo $(curl -X GET --silent https://dnsleaktest.com | grep Hello) > tmp"
-WAN_LOCATION_COMMAND = "echo $(curl -X GET --silent https://dnsleaktest.com | grep from) > tmp"
-MAC_COMMAND = f"echo $(macchanger --show {INTERFACE}) > tmp"
+LAN_IP_COMMAND = f"echo $(ifconfig {INTERFACE} | grep inet) > tmp.info"
+WAN_IP_COMMAND = "echo $(curl -X GET --silent https://dnsleaktest.com | grep Hello) > tmp.info"
+MAC_COMMAND = f"echo $(macchanger --show {INTERFACE}) > tmp.info"
 
 def get_info(command: str, filter_index: int) -> str:
 	required = None
-	with open("tmp", "r") as file:
+	with open("tmp.info", "r") as file:
 		data = file.read()
 		result = str(data).split()
 		required = result[filter_index]
+		file.close()
+	system("rm tmp.info")
 	return str(required)
+
+def get_wan_location() -> dict :
+    location = {}
+    ip = sanitize(string=get_info(command=system(WAN_IP_COMMAND), filter_index=2), split_character='<', filter_index=0)
+    system(f"curl -X GET --silent http://ip-api.com/json/{ip} > loc.info")
+    with open("loc.info", "r") as file :
+        data = loads(file.read())
+        location = {
+			'city': data['city'],
+			'country': data['country']
+		}
+        file.close()
+    system("rm loc.info")
+    return location
 	
 def macs () -> list :
 	CURRENT = get_info(system(MAC_COMMAND), 2)
@@ -22,21 +38,17 @@ def macs () -> list :
 def sanitize(string: str, split_character: chr or str, filter_index: int) -> str :
 	return string.split(split_character)[filter_index]
 
-try :
-	VARS = {
-		# "lan": get_info(command=system(LAN_IP_COMMAND), filter_index = 1),
-		"wan": sanitize(string=get_info(command=system(WAN_IP_COMMAND), filter_index=2), split_character='<', filter_index=0),
-		"location": sanitize(string=get_info(command=system(WAN_LOCATION_COMMAND), filter_index=2), split_character='<', filter_index=0),
-		# "current_mac": macs()[0],
-		# "permanent_mac": macs()[1],
-	}
-except Exception as e :
-	print(f"An error occured : {e}")
+VARS = {
+	# "lan": get_info(command=system(LAN_IP_COMMAND), filter_index = 1),
+	"wan": sanitize(string=get_info(command=system(WAN_IP_COMMAND), filter_index=2), split_character='<', filter_index=0),
+	"location": f"{get_wan_location()['city']}, {get_wan_location()['country']}" 
+	# "current_mac": macs()[0],
+	# "permanent_mac": macs()[1],
+}
 
 def myipinfo() -> None :
     system("clear")
     for key, value in VARS.items() :
         print(f"{key} : {value}")
-    system("rm tmp")
 
 myipinfo()
